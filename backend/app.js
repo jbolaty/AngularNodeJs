08aeb064fc97f635fbf1441a461c575dd73361b2
jbolaty <jeanmarcbolaty@yahoo.fr>
2020-05-15 16:20:32 +0200

    Add and API /api/posts

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,24 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('first middleware');
-    next();
-});
-
-app.use((req, res, next) => {
-    res.send('Hello from express');
+app.use('/api/posts',(req, res, next) => {
+    const posts = [
+        {
+        id: 'fadf12421l',
+        title : 'first server-side post',
+        content : 'this is comming from the server'
+    },
+    {
+        id: 'ksaf12421l',
+        title : 'second server-side post',
+        content : 'this is comming from the server!'
+    },
+    ]
+    
+    res.status(200).json({
+        message: 'posts fetched successfully',
+        posts: posts
+    });
 })
 
 module.exports = app;
